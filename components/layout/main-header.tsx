8e4b1914efa7093a268d639ca744504a93af26ca
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-05 13:17:01 +0330

    wrap app with layout

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -3,14 +3,16 @@ import React from "react";
 
 const MainHeader = () => {
   return (
-    <header className="w-full flex justify-between item-baseline px-20 py-[10%] h-20 bg-trueGray-800">
+    <header className="w-full flex justify-between items-center px-20 py-[1%] h-20 bg-black">
       <div className="text-2xl h-full flex justify-center items-center text-cyan-200 md:text-[2.5rem]">
         <Link href={"/"}>Next event</Link>
       </div>
       <nav className="no-underline text-teal-300 text-base">
         <ul>
           <li>
-            <Link href={"/events"} className="no-underline md:text-2xl ">Browse All Events</Link>
+            <Link href={"/events"} className="no-underline md:text-2xl ">
+              Browse All Events
+            </Link>
           </li>
         </ul>
       </nav>
