a4edad0a6e2d46ce1e53c202d59ead57c28c03b4
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-05 16:57:43 +0330

    feat:event page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,8 +1,13 @@
+import EventList from '@/components/events/event-list'
+import { getAllEvents } from '@/dummy-data'
 import React from 'react'
 
 const AllEventsPage = () => {
+  const events=getAllEvents()
   return (
-    <div>AllEventsPage</div>
+    <div>
+      <EventList items={events} />
+    </div>
   )
 }
 
