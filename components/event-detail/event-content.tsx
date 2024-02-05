a6319568297e5fd94e066e2a0bf028634afc97a2
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-05 12:09:01 +0330

    add typescript and fix style

---
+++ components/event-detail/event-content.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react';
+
+interface IEventContent{
+  children:ReactNode;
+}
+
+const EventContent:FC<IEventContent>=({children})=> {
+  return (
+    <section className="tetx-2xl text-[#3a3a3a] w-11/12 max-w-[40em] m-auto mt-32 text-center">
+      {children}
+    </section>
+  );
+}
+
+export default EventContent;
