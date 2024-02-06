e1a5bf8c160af5d559fd6b78c7be0e3ec83fba84
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-06 10:57:16 +0330

    improvwe button

--- components/events/event-showDetails.tsx
+++ components/events/event-showDetails.tsx
@@ -1,10 +1,17 @@
 import React, { FC, ReactNode } from "react";
+import Button from "../ui/button";
 interface IShowDetails {
   children: ReactNode;
+  link?: string;
 }
 
-const ShowDetails: FC<IShowDetails> = ({ children }) => {
-  return <div className="flex justify-center text-4xl pt-8">{children}</div>;
+const ShowDetails: FC<IShowDetails> = ({ children, link }) => {
+  return (
+    <div className="flex justify-center text-4xl pt-8 flex-col items-center gap-5">
+      {children}
+      {link && <Button styles={"text-base"} link={link}>Show AllEvents</Button>}
+    </div>
+  );
 };
 
 export default ShowDetails;
