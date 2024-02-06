bb9ce03849e9b54efdb1c7541a8e14ed67ada40b
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-06 11:45:19 +0330

    improve header

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -5,13 +5,13 @@ const MainHeader = () => {
   return (
     <header className="w-full flex justify-between items-center px-20 py-[1%] h-20 bg-black">
       <div className="text-2xl h-full flex justify-center items-center text-cyan-200 md:text-[2.5rem]">
-        <Link href={"/"}>Next event</Link>
+        <Link href={"/"}>HOME</Link>
       </div>
       <nav className="no-underline text-teal-300 text-base">
         <ul>
           <li>
             <Link href={"/events"} className="no-underline md:text-2xl ">
-              Browse All Events
+               All Events
             </Link>
           </li>
         </ul>
