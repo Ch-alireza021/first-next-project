f8fa2d3948c7ad9587a5d32b477352379ffcc1a1
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-07 18:42:27 +0330

    change name to api

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -29,3 +29,21 @@ export const getEventById = async (id: string | undefined) => {
   const allEvents = await getAllEvents();
   return allEvents.find((event) => event.id === id);
 };
+
+
+export const getFilteredEvents = async(dateFilter: {
+  year: number;
+  month: number;
+}) => {
+  const { year, month } = dateFilter;
+  const allEvents=await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+};
