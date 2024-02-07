8e510bd0289372c7ce526dbe7de87beaec17d3ec
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-07 12:14:53 +0330

    satyle eventId

--- components/event-detail/event-summary.tsx
+++ components/event-detail/event-summary.tsx
@@ -7,7 +7,7 @@ interface IEventSummary {
 const EventSummary: FC<IEventSummary> = ({ title }) => {
   return (
     <section className="w-full h-[30vh] max-h-[30vw] bg-gradient-to-bl from-teal-600 to-cyan-700">
-      <h1 className="md:text-[4vw] text-[7vw] text-center text-white pt-[4vh]  ">{title}</h1>
+      <h1 className="md:text-[5vw] text-[7vw] text-center text-white pt-[4vh]  ">{title}</h1>
     </section>
   );
 };
